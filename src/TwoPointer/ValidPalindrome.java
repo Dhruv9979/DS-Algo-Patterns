c76fd858221c0f027b6b68538a5c2bab8a9b6731
Dhruv9979 <starb9979@gmail.com>
2023-08-05 01:11:27 -0400

    Created project

---
+++ src/TwoPointer/ValidPalindrome.java
@@ -0,0 +1,11 @@
+package TwoPointer;
+
+public class ValidPalindrome {
+    public static boolean isPalindrome(String s) { // O(n)
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("ababa"));
+    }
+}
