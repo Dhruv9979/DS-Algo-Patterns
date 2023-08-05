307ba6b059d2a11483cbd8966f73b131981ce4df
Dhruv9979 <starb9979@gmail.com>
2023-08-05 14:57:10 -0400

    Valid Palindrome solution

--- src/TwoPointer/ValidPalindrome.java
+++ src/TwoPointer/ValidPalindrome.java
@@ -1,11 +1,29 @@
 package TwoPointer;
 
 public class ValidPalindrome {
-    public static boolean isPalindrome(String s) { // O(n)
-        return false;
+    public static boolean isPalindrome(String s) {
+        if (s == null || s.length() == 0) return true;// O(n)
+        int i = 0;
+        int j = s.length() - 1;
+        while (i < j) {
+            if (s.charAt(i) == s.charAt(j)) {
+                i++;
+                j--;
+            } else {
+                return false;
+            }
+        }
+        return true;
     }
 
     public static void main(String[] args) {
-        System.out.println(isPalindrome("ababa"));
+        String[] testCase = {"RACEACAR", "A", "ABCDEFGFEDCBA", "ABC", "ABCBA", "ABBA", "RACEACAR"};
+        for (int k = 0; k < testCase.length; k++) {
+            System.out.println("Test Case #" + (k + 1));
+            System.out.println(new String(new char[100]).replace('\0', '-'));
+            System.out.println("The input string is " + testCase[k] + "' and the length of the string is " + testCase[k].length() + ".");
+            System.out.println("\nIs it a palindrome?..... " + isPalindrome(testCase[k]));
+            System.out.println(new String(new char[100]).replace('\0', '-'));
+        }
     }
 }
